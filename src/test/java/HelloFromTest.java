66927a5c4f8d45e82c16334aa4590655136bf61e
ElenaShapoval <Elena.Shapoval@syncretis.com>
2022-09-16 17:12:02 +0300

    code review

--- src/test/java/HelloFromTest.java
+++ src/test/java/HelloFromTest.java
@@ -1,8 +1,30 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloFromTest {
     @Test
     public void testHelloFrom() {
         System.out.println("Hello from Elena Shapoval");
+    }
+
+    @Test
+    public void testRestAssured() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name","Helena");
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.get("answer2");
+        if (answer == null){
+            System.out.println("The key 'answer2' is absent");
+        } else {
+            System.out.println(answer);
+        }
     }
 }
